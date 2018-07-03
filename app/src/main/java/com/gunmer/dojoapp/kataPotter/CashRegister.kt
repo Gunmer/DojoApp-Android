d385a288a32deeedef763dd8da3bdb3d33086e63
Cristiam Sosa <cristiam.sosa@innocv.com>
2018-07-03 18:01:55 +0200

    Add test cases

--- app/src/main/java/com/gunmer/dojoapp/kataPotter/CashRegister.kt
+++ app/src/main/java/com/gunmer/dojoapp/kataPotter/CashRegister.kt
@@ -3,15 +3,28 @@ package com.gunmer.dojoapp.kataPotter
 class CashRegister {
 
     fun calculatePriceOf(shoppingBasket: Array<Book>): Double {
+        val distinctList = shoppingBasket.distinctBy { it.type }
+        val equalsList = shoppingBasket.filter { !distinctList.contains(it) }
 
-        val distinctSize = shoppingBasket.distinctBy { it.type }.size
+        val discount = calculateDiscount(distinctList)
 
-        val discount = when (distinctSize) {
-            2 -> 0.05
-            else -> 0.0
-        }
+        val totalWithDiscount = distinctList.sumByDouble { applyDiscount(it, discount) }
+        val totalWithoutDiscount = equalsList.sumByDouble { it.price }
 
-        return shoppingBasket.sumByDouble { it.price - it.price * discount }
+        return  totalWithDiscount + totalWithoutDiscount
     }
 
+    private fun calculateDiscount(distinctList: List<Book>): Double {
+        return when (distinctList.size) {
+            2 -> 0.05
+            3 -> 0.1
+            4 -> 0.2
+            5 -> 0.25
+            6 -> 0.3
+            7 -> 0.35
+            else -> 0.0
+        }
+    }
+
+    private fun applyDiscount(book: Book, discount: Double) = book.price - book.price * discount
 }
--- app/src/test/java/com/gunmer/dojoapp/kataPotter/CashRegisterTest.kt
+++ app/src/test/java/com/gunmer/dojoapp/kataPotter/CashRegisterTest.kt
@@ -22,7 +22,7 @@ class CashRegisterTest {
     }
 
     @Test
-    fun`should be 8 x n when n is the number of equals books`() {
+    fun `should be 8 x n when n is the number of equals books`() {
         val n = 400
 
         val shoppingBasket = mutableListOf<Book>()
@@ -43,5 +43,113 @@ class CashRegisterTest {
 
         assertThat(price, `is`(15.2))
     }
+
+    @Test
+    fun `should be apply discount to two books whe basket has two distinct books and the others are the same type`() {
+        val shoppingBasket = arrayOf(
+                Book(BookType.PHILOSOPHERS_STONE),
+                Book(BookType.PHILOSOPHERS_STONE),
+                Book(BookType.CHAMBER_OF_SECRETS)
+        )
+
+        val price = cashRegister.calculatePriceOf(shoppingBasket)
+
+        assertThat(price, `is`(23.2))
+
+    }
+
+    @Test
+    fun `should be apply 10% of discount when basket has 3 distinct books`() {
+        val shoppingBasket = arrayOf(
+                Book(BookType.PHILOSOPHERS_STONE),
+                Book(BookType.CHAMBER_OF_SECRETS),
+                Book(BookType.PRISONER_OF_AZKABAN)
+        )
+
+        val price = cashRegister.calculatePriceOf(shoppingBasket)
+
+        assertThat(price, `is`(21.6))
+    }
+
+    @Test
+    fun `should be apply 20% of discount when basket has 4 distinct books`() {
+        val shoppingBasket = arrayOf(
+                Book(BookType.PHILOSOPHERS_STONE),
+                Book(BookType.CHAMBER_OF_SECRETS),
+                Book(BookType.PRISONER_OF_AZKABAN),
+                Book(BookType.ORDER_OF_THE_PHOENIX)
+        )
+
+        val price = cashRegister.calculatePriceOf(shoppingBasket)
+
+        assertThat(price, `is`(25.6))
+    }
+
+    @Test
+    fun `should be apply 25% of discount when basket has 5 distinct books`() {
+        val shoppingBasket = arrayOf(
+                Book(BookType.PHILOSOPHERS_STONE),
+                Book(BookType.CHAMBER_OF_SECRETS),
+                Book(BookType.PRISONER_OF_AZKABAN),
+                Book(BookType.ORDER_OF_THE_PHOENIX),
+                Book(BookType.GOBLET_OF_FIRE)
+        )
+
+        val price = cashRegister.calculatePriceOf(shoppingBasket)
+
+        assertThat(price, `is`(30.0))
+    }
+
+    @Test
+    fun `should be apply 30% of discount when basket has 6 distinct books`() {
+        val shoppingBasket = arrayOf(
+                Book(BookType.PHILOSOPHERS_STONE),
+                Book(BookType.CHAMBER_OF_SECRETS),
+                Book(BookType.PRISONER_OF_AZKABAN),
+                Book(BookType.ORDER_OF_THE_PHOENIX),
+                Book(BookType.GOBLET_OF_FIRE),
+                Book(BookType.HALF_BLOOD_PRINCE)
+        )
+
+        val price = cashRegister.calculatePriceOf(shoppingBasket)
+
+        assertThat(price, `is`(33.6))
+    }
+
+    @Test
+    fun `should be apply 35% of discount when basket has 7 distinct books`() {
+        val shoppingBasket = arrayOf(
+                Book(BookType.PHILOSOPHERS_STONE),
+                Book(BookType.CHAMBER_OF_SECRETS),
+                Book(BookType.PRISONER_OF_AZKABAN),
+                Book(BookType.ORDER_OF_THE_PHOENIX),
+                Book(BookType.GOBLET_OF_FIRE),
+                Book(BookType.HALF_BLOOD_PRINCE),
+                Book(BookType.DEATHLY_HALLOWS)
+        )
+
+        val price = cashRegister.calculatePriceOf(shoppingBasket)
+
+        assertThat(price, `is`(36.4))
+    }
+
+    @Test
+    fun `should be apply 5% of discount when basket has 7 distinct books`() {
+        val shoppingBasket = arrayOf(
+                Book(BookType.PRISONER_OF_AZKABAN),
+                Book(BookType.ORDER_OF_THE_PHOENIX),
+                Book(BookType.PRISONER_OF_AZKABAN),
+                Book(BookType.ORDER_OF_THE_PHOENIX),
+                Book(BookType.PRISONER_OF_AZKABAN),
+                Book(BookType.ORDER_OF_THE_PHOENIX),
+                Book(BookType.PRISONER_OF_AZKABAN),
+                Book(BookType.ORDER_OF_THE_PHOENIX)
+        )
+
+        val price = cashRegister.calculatePriceOf(shoppingBasket)
+
+        assertThat(price, `is`(12.2))
+    }
+
 
 }
