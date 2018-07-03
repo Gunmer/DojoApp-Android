32b755eb1a514edb5a0571d18928b0e7239afbd6
Cristiam Sosa <cristiam.sosa@innocv.com>
2018-07-03 17:03:33 +0200

    Add first case test

--- app/src/main/java/com/gunmer/dojoapp/kataPotter/CashRegister.kt
+++ app/src/main/java/com/gunmer/dojoapp/kataPotter/CashRegister.kt
@@ -2,8 +2,16 @@ package com.gunmer.dojoapp.kataPotter
 
 class CashRegister {
 
-    fun calculatePriceOf(shoppingBasket: Array<Book>) : Double {
-        throw RuntimeException("Un implemented function")
+    fun calculatePriceOf(shoppingBasket: Array<Book>): Double {
+
+        val distinctSize = shoppingBasket.distinctBy { it.type }.size
+
+        val discount = when (distinctSize) {
+            2 -> 0.05
+            else -> 0.0
+        }
+
+        return shoppingBasket.sumByDouble { it.price - it.price * discount }
     }
 
 }
--- app/src/test/java/com/gunmer/dojoapp/kataPotter/CashRegisterTest.kt
+++ app/src/test/java/com/gunmer/dojoapp/kataPotter/CashRegisterTest.kt
@@ -21,4 +21,27 @@ class CashRegisterTest {
         assertThat(price, `is`(8.0))
     }
 
+    @Test
+    fun`should be 8 x n when n is the number of equals books`() {
+        val n = 400
+
+        val shoppingBasket = mutableListOf<Book>()
+        for (i in 1..n) {
+            shoppingBasket.add(Book(BookType.PHILOSOPHERS_STONE))
+        }
+
+        val price = cashRegister.calculatePriceOf(shoppingBasket.toTypedArray())
+
+        assertThat(price, `is`(8.0 * n))
+    }
+
+    @Test
+    fun `should be apply 5% of discount when basket has 2 distinct books`() {
+        val shoppingBasket = arrayOf(Book(BookType.PHILOSOPHERS_STONE), Book(BookType.CHAMBER_OF_SECRETS))
+
+        val price = cashRegister.calculatePriceOf(shoppingBasket)
+
+        assertThat(price, `is`(15.2))
+    }
+
 }
